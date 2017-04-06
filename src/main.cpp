32752544a8a209321a2da8bde83e3657d56d2d35
Devildrake <davidsierragonzalez@enti.cat>
2017-04-06 11:45:41 +0200

    Establecida la clase camara, ahora la matriz view es el metodo LookAt de la clase camara

--- src/main.cpp
+++ src/main.cpp
@@ -17,163 +17,157 @@ bool WIDEFRAME = false;
 bool paintQuad = false;
 bool fade1 = false;
 float mixStuff;
-float rotacionX,rotacionY = 0.0f;
+float rotacionY = 0.0f;
+float rotacionX = -90.0f;
 float gradosRot = 0;
 float aumentoRot;
+bool aumentarRotRight, aumentarRotLeft, aumentarUp, aumentarDown;
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void cursor_callback(GLFWwindow* window,double xPos, double yPos);
 void scroll_callback(GLFWwindow* window,double xOffset, double yOffset);
-void DoMovement(GLFWwindow* window);
-mat4 miLookAt(vec3 camPos,vec3 whereToLook, vec3 worldUp);
-bool aumentarRotRight, aumentarRotLeft,aumentarUp,aumentarDown;
-bool moveForward, moveBackwards, moveRight, moveLeft;
-double mouseLastPosX,mouseLastPosY;
-bool start;
-bool runFaster;
-float Pitch, Yaw;
-float sensibilidad;
-float FOV = 45.0f;
-vec3 cameraPos;
-vec3 cameraDir;
-vec3 cameraRight;
-float prevTime = 0;
-float cameraSpeed = 0.5f;
 
-
-mat4 c;
-//cout <<"CameraUpA"<< cameraUp.x<<", "<< cameraUp.y <<", "<< cameraUp.z << endl;
-mat4 d;
-//cout << "CameraUpB" << cameraUp.x << ", " << cameraUp.y << ", " << cameraUp.z << endl;
 #pragma region Clase Camera
-//class Camera
-//{
-//public:
-//	Camera(vec3 position, vec3 direction, GLfloat sensitivity, GLfloat fov);
-//	void DoMovement(GLFWwindow * window);
-//	void MouseMove(GLFWwindow* window, double xpos, double ypos);
-//	void MouseScroll(GLFWwindow* window, double xScroll, double yScroll);
-//	mat4 LookAt();
-//	GLfloat GetFOV();
-//
-//
-//private:
-//	vec3 cameraPos;
-//	vec3 cameraFront;
-//	vec3 cameraUp;
-//	GLfloat Deltatime;
-//	GLfloat Lastframe;
-//	GLfloat LastMx;
-//	GLfloat LastMy;
-//	GLfloat Sensitivity;
-//	GLboolean firstMouse;
-//	GLfloat PITCH;
-//	GLfloat YAW;
-//	GLfloat FOV;
-//};
-//
-//Camera::Camera(vec3 position, vec3 direction, GLfloat sensitivity, GLfloat fov)
-//{
-//	Lastframe = 0;
-//	cameraPos = position;	
-//	cameraFront = direction;
-//	Sensitivity = sensitivity;
-//	FOV = fov;
-//}
-//
-//void Camera::DoMovement(GLFWwindow * window) {
-//
-//	Deltatime = glfwGetTime() - Lastframe;
-//	Lastframe = glfwGetTime();
-//
-//	float cameraSpeed = 0.5f;
-//
-//		bool backwards = glfwGetKey(window, GLFW_KEY_W);
-//		bool forward = glfwGetKey(window, GLFW_KEY_S);
-//		bool left = glfwGetKey(window, GLFW_KEY_A);
-//		bool right = glfwGetKey(window, GLFW_KEY_D);
-//
-//		if (forward) {
-//			cameraPos.z -= normalize(cameraFront).z*cameraSpeed*Deltatime;
-//		}
-//		else if (backwards) {
-//			cameraPos.z += normalize(cameraFront).z*cameraSpeed*Deltatime;
-//		}
-//		if (left) {
-//			cameraPos.x += (cross(vec3(0, 1, 0), (normalize(cameraFront)))).x*cameraSpeed*Deltatime;
-//		}
-//		else if (right) {
-//			cameraPos.x -= (cross(vec3(0, 1, 0), (normalize(cameraFront)))).x*cameraSpeed*Deltatime;
-//		}
-//}
-//void Camera::MouseMove(GLFWwindow* window, double xpos, double ypos) {
-//		double offsetX, offsetY;
-//		if (!start) {
-//			YAW = 270.0f;
-//			PITCH = 0.0f;
-//			start = true;
-//			LastMx = cameraPos.x;
-//			LastMy= cameraPos.y;
-//		}
-//		offsetX = cameraPos.x - LastMx;
-//		offsetY = cameraPos.y - LastMy;
-//
-//		LastMx = xpos;
-//		LastMy = ypos;
-//
-//
-//		offsetX *= Sensitivity;
-//		offsetY *= Sensitivity;
-//
-//		YAW += offsetX;
-//		Pitch -= offsetY;
-//
-//		Pitch = clamp(PITCH, -89.9f, 89.9f);
-//		YAW = mod(YAW, 360.0f);
-//
-//		cameraFront.x = cos(glm::radians(YAW)) * cos(glm::radians(PITCH));
-//		cameraFront.y = sin(glm::radians(PITCH));
-//		cameraFront.z = sin(glm::radians(YAW)) * cos(glm::radians(PITCH));
-//		cameraFront = normalize(cameraFront);
-//	}
-//
-//void Camera::MouseScroll(GLFWwindow* window, double xScroll, double yScroll) {
-//
-//		if (FOV >= 1.0f && FOV <= 45.0f)
-//			FOV -= yScroll / 10;
-//		if (FOV <= 1.0f)
-//			FOV = 1.0f;
-//		if (FOV >= 45.0f)
-//			FOV = 45.0f;
-//}
-//mat4 Camera::LookAt() {
-//		vec3 zaxis = normalize(cameraFront);
-//		vec3 xaxis = normalize(cross(vec3(0, 1, 0), zaxis));
-//		vec3 yaxis = cross(zaxis, xaxis);
-//		mat4 rotation, translation;
-//		rotation[0][0] = xaxis.x;
-//		rotation[1][0] = xaxis.y;
-//		rotation[2][0] = xaxis.z;
-//		rotation[0][1] = yaxis.x;
-//		rotation[1][1] = yaxis.y;
-//		rotation[2][1] = yaxis.z;
-//		rotation[0][2] = zaxis.x;
-//		rotation[1][2] = zaxis.y;
-//		rotation[2][2] = zaxis.z;
-//		rotation[3][3] = 1;
-//
-//		translation[3][0] = -cameraPos.x;
-//		translation[3][1] = -cameraPos.y;
-//		translation[3][2] = -cameraPos.z;
-//		translation[3][3] = 1;
-//
-//		mat4 res = rotation*translation;
-//
-//		return res;
-//
-//}
-//GLfloat Camera::GetFOV() {
-//	return FOV;
-//}
+
+class Camera
+{
+public:
+	Camera(vec3 position, vec3 direction, GLfloat sensitivity, GLfloat fov);
+	void DoMovement(GLFWwindow * window);
+	void MouseMove(GLFWwindow* window, double xpos, double ypos);
+	void MouseScroll(GLFWwindow* window, double xScroll, double yScroll);
+	mat4 LookAt();
+	GLfloat GetFOV();
+
+
+private:
+	vec3 cameraPos;
+	vec3 cameraFront;
+	vec3 cameraUp;
+	GLfloat Deltatime;
+	GLfloat Lastframe;
+	GLfloat LastMx;
+	GLfloat LastMy;
+	GLfloat Sensitivity;
+	GLboolean firstMouse;
+	GLfloat PITCH;
+	GLfloat YAW;
+	GLfloat FOV;
+};
+
+Camera::Camera(vec3 position, vec3 direction, GLfloat sensitivity, GLfloat fov)
+{
+	Lastframe = 0;
+	cameraPos = position;	
+	cameraFront = direction;
+	Sensitivity = sensitivity;
+	FOV = fov;
+
+	vec3 directionX = normalize(vec3(0,direction.y,direction.z));
+	vec3 z(0,0,1);
+	vec3 directionY = normalize(vec3(direction.x, 0, direction.z));
+
+
+	PITCH = 90 - degrees(acos(dot(vec3(0,1,0),directionX)));
+	YAW = 90 - degrees(acos(dot(directionY, z)));
+}
+
+void Camera::DoMovement(GLFWwindow * window) {
+
+	Deltatime = glfwGetTime() - Lastframe;
+	Lastframe = glfwGetTime();
+
+	float cameraSpeed = 0.5f;
+
+		bool backwards = glfwGetKey(window, GLFW_KEY_W);
+		bool forward = glfwGetKey(window, GLFW_KEY_S);
+		bool left = glfwGetKey(window, GLFW_KEY_A);
+		bool right = glfwGetKey(window, GLFW_KEY_D);
+
+		if (forward) {
+			cameraPos += normalize(cameraFront)*cameraSpeed*Deltatime;
+		}
+		else if (backwards) {
+			cameraPos -= normalize(cameraFront)*cameraSpeed*Deltatime;
+		}
+		if (left) {
+			cameraPos -= (cross(vec3(0, 1, 0), (normalize(cameraFront))))*cameraSpeed*Deltatime;
+		}
+		else if (right) {
+			cameraPos += (cross(vec3(0, 1, 0), (normalize(cameraFront))))*cameraSpeed*Deltatime;
+		}
+}
+
+void Camera::MouseMove(GLFWwindow* window, double xpos, double ypos) {
+		double offsetX, offsetY;
+		if (!firstMouse) {
+			firstMouse = true;
+			LastMx = xpos;
+			LastMy= ypos;
+		}
+		offsetX = xpos - LastMx;
+		offsetY = ypos - LastMy;
+
+		LastMx = xpos;
+		LastMy = ypos;
+
+
+
+		offsetX *= Sensitivity;
+		offsetY *= Sensitivity;
+
+		cout << "ClaseOffsetX " << offsetX << endl;
+		cout << "ClaseOffsetY " << offsetY << endl;
+
+		YAW += offsetX;
+		PITCH += offsetY;
+
+		PITCH = clamp(PITCH, -89.9f, 89.9f);
+		YAW = mod(YAW, 360.0f);
+
+
+		vec3 dir;
+
+		dir.x = cos(glm::radians(YAW)) * cos(glm::radians(PITCH));
+		dir.y = sin(glm::radians(PITCH));
+		dir.z = sin(glm::radians(YAW)) * cos(glm::radians(PITCH));
+
+		cameraFront = normalize(dir);
+	}
+
+void Camera::MouseScroll(GLFWwindow* window, double xScroll, double yScroll) {
+			FOV -= yScroll ;
+			FOV = clamp(FOV, 1.f, 45.f);
+}
+mat4 Camera::LookAt() {
+		vec3 zaxis = normalize(cameraFront);
+		vec3 xaxis = normalize(cross(vec3(0, 1, 0), zaxis));
+		vec3 yaxis = cross(zaxis, xaxis);
+		mat4 rotation, translation;
+		rotation[0][0] = xaxis.x;
+		rotation[1][0] = xaxis.y;
+		rotation[2][0] = xaxis.z;
+		rotation[0][1] = yaxis.x;
+		rotation[1][1] = yaxis.y;
+		rotation[2][1] = yaxis.z;
+		rotation[0][2] = zaxis.x;
+		rotation[1][2] = zaxis.y;
+		rotation[2][2] = zaxis.z;
+		rotation[3][3] = 1;
+
+		translation[3][0] = -cameraPos.x;
+		translation[3][1] = -cameraPos.y;
+		translation[3][2] = -cameraPos.z;
+		translation[3][3] = 1;
+
+		mat4 res = rotation*translation;
+
+		return res;
+
+}
+GLfloat Camera::GetFOV() {
+	return FOV;
+}
 #pragma endregion
 
 void DrawVao(GLuint programID, GLuint VAO) {
@@ -222,27 +216,13 @@ mat4 GenerateModelMatrix(vec3 aTranslation, vec3 aRotation, vec3 CubesPosition, 
 	return temp;
 }
 
-void DoMovement(GLFWwindow* window){
-	float dt = glfwGetTime() - prevTime;
-	prevTime = glfwGetTime();
-	if (glfwGetKey(window, GLFW_KEY_W)) {
-		cameraPos += cameraDir*dt*cameraSpeed;
-	}
-	else if (glfwGetKey(window, GLFW_KEY_S)) {
-		cameraPos -= cameraDir*dt*cameraSpeed;
-	}
-	if (glfwGetKey(window, GLFW_KEY_A)) {
-		cameraPos += cameraRight*dt*cameraSpeed;
-	}
-	else if (glfwGetKey(window, GLFW_KEY_D)) {
-		cameraPos -= cameraRight*dt*cameraSpeed;
-	}
-}
+Camera camara(vec3(0, 0, 3), normalize(vec3(0, 0, -3)), 0.04f, 45.0f);
 
 void main() {
-	runFaster = false;
-	sensibilidad = 0.04f;
-	start = false;
+
+	//runFaster = false;
+	//sensibilidad = 0.04f;
+	//start = false;
 	mixStuff = 0.0f;
 	//initGLFW
 	if (!glfwInit())
@@ -521,66 +501,30 @@ void main() {
 	glEnable(GL_DEPTH_TEST);
 	//bucle de dibujado
 
-	cameraPos = vec3(0,0,3);
-	cameraDir = normalize(vec3(0,0,0)- cameraPos);
-	//cameraDir = vec3(0, 0, -1);
-	cameraRight = normalize(cross(vec3(0, 1, 0), cameraDir));
+	//cameraPos = vec3(0,0,3);
+	//cameraDir = normalize(vec3(0,0,0)- cameraPos);
+	////cameraDir = vec3(0, 0, -1);
+	//cameraRight = normalize(cross(vec3(0, 1, 0), cameraDir));
 
-	vec3 cameraUp = normalize(cross(cameraDir, cameraRight));
+	//vec3 cameraUp = normalize(cross(cameraDir, cameraRight));
 
-	moveForward = moveBackwards = moveRight = moveLeft = false;
-	Pitch = 0;
+	//moveForward = moveBackwards = moveRight = moveLeft = false;
+	//Pitch = 0;
 
 	while (!glfwWindowShouldClose(window))
 	{
-		cameraRight = normalize(cross(vec3(0, 1, 0), cameraDir));
 
-		c = miLookAt(cameraPos, cameraPos + cameraDir, cameraUp);
-
-		d = lookAt(cameraPos, cameraPos + cameraDir, cameraUp);
-
-		if (c == d) {
-			cout << "Confirmamos \n";
-		}
-
-		if (runFaster) {
-			cameraSpeed = 2;
-		}
-		else {
-			cameraSpeed = 0.5f;
-		}
-
-		DoMovement(window);
-
-		//vec3 cameraDir = normalize(vec3(0,0,0)- cameraPos);
-
-
-
-		//mat4 finalMatrix; //Modelo
+		camara.DoMovement(window);
 
 		mat4 cam; //Vista
 		mat4 proj; //Proyeccion
-
-
-		//mat4 proj = perspective(60, screenWithd/screenHeight, 0.1f, 100);
-
 
 		if (gradosRot > 360 || gradosRot < -360) {
 			gradosRot = 0;
 		}
 
-		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
-
-		/*if (loc != -1)
-		{
-		glUniform1f(loc, glfwGetTime());
-		}*/
-
 		glfwPollEvents();
 
-		//matModelID = glGetUniformLocation(shader.Program, "matrizModel");
-		//matProjID = glGetUniformLocation(shader.Program, "matrizProj");
-		//matViewID = glGetUniformLocation(shader.Program, "matrizView");
 		matrizDefID = glGetUniformLocation(shader.Program, "matrizDefinitiva");
 
 		//Establecer el color de fondo
@@ -596,18 +540,6 @@ void main() {
 		
 		glUniform1f(mixID, mixStuff);
 
-
-		//glClearColor(0, 1, 0, 0);
-
-		//DrawVao(shader.Program,VAO);
-		//	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		//glDrawElements(GL_POLYGON, 6, GL_UNSIGNED_INT, 0);
-
-
-		//glBindVertexArray(0);
-
-		//glBindTexture(GL_TEXTURE_2D, texture1);
-
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glUniform1i(locTex,0);
@@ -617,25 +549,7 @@ void main() {
 		glUniform1i(locTex2, 1);
 		
 		glBindVertexArray(VAO);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-		
-		
-		//pintar con lineas
-		//pintar con triangulos
-
-		//if (moveForward) {
-		//	cameraPos.z -= normalize(cameraDir).z*cameraSpeed*dt;
-		//}
-		//else if (moveBackwards) {
-		//	cameraPos.z += normalize(cameraDir).z*cameraSpeed*dt;
-		//}
-		//if (moveLeft) {
-		//	cameraPos.x+= normalize(cameraRight).x*cameraSpeed*dt;
-		//}
-		//else if (moveRight) {
-		//	cameraPos.x -= normalize(cameraRight).x*cameraSpeed*dt;
-		//}
 
 		if (aumentarRotLeft) {
 			rotacionY-=aumentoRot;
@@ -662,12 +576,9 @@ void main() {
 			}
 		}
 
-		proj = perspective(FOV, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+		proj = perspective(radians(camara.GetFOV()), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
 
-		cam = lookAt(cameraPos, cameraPos + cameraDir, cameraUp);
-
-		//glUniformMatrix4fv(matProjID, 1, GL_FALSE, glm::value_ptr(proj));
-		//glUniformMatrix4fv(matViewID, 1, GL_FALSE, glm::value_ptr(cam));
+		cam = camara.LookAt();
 
 		for (int i = 0; i < 10; i++) {
 			//Model Matrix
@@ -682,11 +593,10 @@ void main() {
 				rotot = (int)rotot % 360;
 				modelMatrix = GenerateModelMatrix(vec3(0.0f), vec3(1, 0.5f, 0), CubesPositionBuffer[i], rotot);
 			}
-			//glUniformMatrix4fv(matModelID, 1, GL_FALSE, glm::value_ptr(matriz));
 
 			mat4 matrizDefinitiva;
 
-			matrizDefinitiva = proj*cam*modelMatrix;
+			matrizDefinitiva = proj*camara.LookAt()*modelMatrix;
 
 			glUniformMatrix4fv(matrizDefID, 1, GL_FALSE, glm::value_ptr(matrizDefinitiva));
 
@@ -738,60 +648,17 @@ mat4 miLookAt(vec3 camPos, vec3 whereToLook, vec3 cameraUp) {
 }
 
 void cursor_callback(GLFWwindow* window, double xPos, double yPos){
-	double offsetX, offsetY;
-	if (!start) {
-		Yaw = 270.0f;
-		Pitch = 0.0f;
-		start = true;
-		mouseLastPosX = xPos;
-		mouseLastPosY = yPos;
-	}
-		offsetX = xPos - mouseLastPosX;
-		offsetY = yPos - mouseLastPosY;
-
-		mouseLastPosX = xPos;
-		mouseLastPosY = yPos;
-		
-
-		offsetX *= sensibilidad;
-		offsetY *= sensibilidad;
-
-		Yaw += offsetX;
-		Pitch -= offsetY;
-
-		Pitch = clamp(Pitch,-89.9f,89.9f);
-		Yaw = mod(Yaw,360.0f);
-
-		vec3 front;
-
-		front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-		front.y = sin(glm::radians(Pitch));
-		front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-		cameraDir = normalize(front);
-
+		camara.MouseMove(window, xPos, yPos);
 }
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-	if (FOV >= 1.0f && FOV <= 45.0f)
-		FOV -= yoffset/10;
-	if (FOV <= 1.0f)
-		FOV = 1.0f;
-	if (FOV >= 45.0f)
-		FOV = 45.0f;
+	camara.MouseScroll(window, xoffset, yoffset);
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
 	//TODO, comprobar que la tecla pulsada es escape para cerrar la aplicación y la tecla w para cambiar a modo widwframe
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
-
-	if (key == GLFW_KEY_LEFT_SHIFT&&action == GLFW_PRESS) {
-		runFaster = true;
-	}
-
-	if (key == GLFW_KEY_E&&action == GLFW_PRESS) {
-		PrintAndCompareMatrix4(c,d);
-	}
 
 	if (key == GLFW_KEY_UP&&action == GLFW_PRESS) {
 		aumentarUp = true;
